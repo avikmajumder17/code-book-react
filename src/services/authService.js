b55a0f5ab65245facc093364ede985df5627f366
Avik Majumder <avikmajumder17@gmail.com>
2025-06-10 00:56:58 +0530

    eslint connect eslint

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,7 +7,7 @@ export async function login(authDetail) {
 
     const response = await fetch(`${process.env.REACT_APP_HOST}/login`, responseDetails);
     if (!response.ok) {
-        throw { message: response.statusText, status: response.status };
+        throw { message: response.statusText, status: response.status }; //eslint-disable-line
     }
     const data = await response.json();    
 
--- src/services/productService.js
+++ src/services/productService.js
@@ -2,7 +2,7 @@ export async function getProductList(searchTerm) {
     const URL = `${process.env.REACT_APP_HOST}/444/products?name_like=${searchTerm ? searchTerm : ""}`;
     const response = await fetch(URL);
     if (!response.ok) {
-        throw { message: response.statusText, status: response.status };
+        throw { message: response.statusText, status: response.status }; //eslint-disable-line
     }
     const data = await response.json();
 
@@ -12,7 +12,7 @@ export async function getProductList(searchTerm) {
 export async function getProduct(id) {
     const response = await fetch(`${process.env.REACT_APP_HOST}/444/products/${id}`);
     if (!response.ok) {
-        throw { message: response.statusText, status: response.status };
+        throw { message: response.statusText, status: response.status }; //eslint-disable-line
     }
     const data = await response.json();
 
@@ -23,7 +23,7 @@ export async function getFeaturedProduct() {
     const URL = `${process.env.REACT_APP_HOST}/444/featured_products`;
     const response = await fetch(URL);
     if (!response.ok) {
-        throw { message: response.statusText, status: response.status };
+        throw { message: response.statusText, status: response.status }; //eslint-disable-line
     }
     const data = await response.json();
 
